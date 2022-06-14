cb04e69d6607c37a7bc3a7437404a570d5707984
Angel Chavez <angelgce.chavez@gmail.com>
2022-06-14 17:26:31 -0400

    lambda filter

--- models/Pagos.java
+++ models/Pagos.java
@@ -2,8 +2,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
-// import java.util.concurrent.atomic.AtomicBoolean;
-// import java.util.concurrent.atomic.AtomicInteger;
 
 public class Pagos {
 
